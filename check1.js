166b5b7672e31a8fc730eab7bb8667c34b399ccd
OtmaneBENS <134215797+OtmaneBENS@users.noreply.github.com>
2024-09-17 10:12:21 +0200

    Add files via upload

---
+++ check1.js
@@ -0,0 +1,7 @@
+function check1(req, res, next) {
+    if (!req.query.username) {
+        return res.status(400).json({ message: 'Paramètre "username" manquant' });
+    }
+    next();
+}
+module.exports = check1;
